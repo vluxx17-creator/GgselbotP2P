8f4fa7f5bab20aa81b0ad312aea191a0d52e04eb
DiMaggio <vluxx17@gmail.com>
2026-07-14 17:14:16 +0300

    Fix typo in BOT_TOKEN variable name

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "8494220705:AAH68tF-kA6yqoxFDr0QSUZ3LlkadxZPSJw"
+BOT_TOKENBOT_TOKEN = "8494220705:AAH68tF-kA6yqoxFDr0QSUZ3LlkadxZPSJw"
 ADMIN_IDS = [8400055743]
 MINI_APP_URL = "https://ggselbotp2p.onrender.com"  # замените на URL вашего мини-аппа
