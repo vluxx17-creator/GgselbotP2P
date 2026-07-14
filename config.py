44cfc200fc64f03cfc4fc77844617c5ba33b990a
DiMaggio <vluxx17@gmail.com>
2026-07-14 16:11:09 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = "8494220705:AAH68tF-kA6yqoxFDr0QSUZ3LlkadxZPSJw"
+ADMIN_IDS = [8400055743]  # Ваш ID
+DATABASE_URL = "sqlite:///data.db"
+
+# URL для мини-приложения (замените на ваш после деплоя)
+MINI_APP_URL = "https://ваш-домен.com"  # Например: https://ggsel-bot.onrender.com
