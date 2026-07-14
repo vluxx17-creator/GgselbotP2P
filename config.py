63b16de9757f2144acec63496c722ca2f74c04c6
DiMaggio <vluxx17@gmail.com>
2026-07-14 16:28:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-import os
-
 BOT_TOKEN = "8494220705:AAH68tF-kA6yqoxFDr0QSUZ3LlkadxZPSJw"
-ADMIN_IDS = [8400055743]  # Ваш ID
-DATABASE_URL = "sqlite:///data.db"
-
-# URL для мини-приложения (замените на ваш после деплоя)
-MINI_APP_URL = "https://ggselbotp2p.onrender.com"  # Например: https://ggsel-bot.onrender.com
+ADMIN_IDS = [8400055743]
+MINI_APP_URL = "https://ggselbotp2p.onrender.com"  # замените на URL вашего мини-аппа
